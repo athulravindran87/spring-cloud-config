9d8576b2e31f1bfc8ddf1778488045a41488d31e
Athul Ravindran <prasanna29387@gmail.com>
2020-03-10 16:15:50 -0400

    new controller

--- config-server/src/main/java/controller/RefreshController.java
+++ config-server/src/main/java/controller/RefreshController.java
@@ -7,11 +7,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Objects;
 
+@RestController
 @RequestMapping(value = "/refresh")
 public class RefreshController {
 
