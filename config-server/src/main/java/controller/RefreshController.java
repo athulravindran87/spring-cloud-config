c57865539ac3996831c65f4890396e843e7c4121
Athul Ravindran <prasanna29387@gmail.com>
2020-03-10 16:29:53 -0400

    add string post body

--- config-server/src/main/java/controller/RefreshController.java
+++ config-server/src/main/java/controller/RefreshController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,12 +17,14 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/refresh")
+@Slf4j
 public class RefreshController {
 
 
     @PostMapping(value = "/git", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> gitRefresh() throws Exception {
+    public ResponseEntity<String> gitRefresh(@RequestBody String payload) throws Exception {
 
+        log.info("Payload {}", payload);
         String clientIp = System.getenv("CONFIG_SERVICE_SERVICE_HOST");
         String port = System.getenv("CONFIG_SERVICE_SERVICE_PORT_CONFIG_SERVER_PORT");
 
